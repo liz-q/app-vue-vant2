dded11ed861655e82449741d689829cfc4bc11f5
Zhengzheng123 <1595397386@qq.com>
2019-02-15 15:54:40 +0800

    2.15

--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
     mine
+    <Input v-model="inputValue" />
+    <div>{{inputValue}}</div>
     <BottomBar />
   </div>
 </template>
 
 <script>
+  import Input from '../components/Input'
   export default {
-    name: "Mine"
+    name: "Mine",
+    data(){
+      return {
+        inputValue:'2323'
+      }
+    },
+    components:{
+      Input,
+    },
+    methods:{
+      inputKeyUp(){
+        console.log(this.inputValue);
+      }
+    }
   }
 </script>
 
