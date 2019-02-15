a7c0a6f2225c5b0c7eb5cd220979014112a9c47d
Zhengzheng123 <1595397386@qq.com>
2019-02-15 15:53:16 +0800

    2.15

---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="updateVal($event.target.value)">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Input",
+    props:[
+      'value'
+    ],
+    methods:{
+      updateVal(val){
+        console.log(val);
+        this.$emit('input',val)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
